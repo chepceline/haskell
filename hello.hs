209ae85dbd1cb96e8683f7a94cbe75df9122d71c
celine <chepceline25@gmail.com>
2022-12-06 02:46:07 +0300

    write long words

--- hello.hs
+++ hello.hs
@@ -1,8 +1,4 @@
 main = do
-   putStrLn "Hello World"
-   putStrLn "Hello Dama"
-   putStrLn "Hello Millie"
-main = do
-    putStrLn "|"
-    putStrLn "|"
-    putStrLn "|"
+    putStrLn "|  |  "
+    putStrLn "|--|  "
+    putStrLn "|  |  "
